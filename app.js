48f8e898ea206c347b5cf4263f994817767ae10b
CRM_DEV <7204@bmi.io>
2018-04-14 20:57:35 +0300

    new

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-/*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
-
 var restify = require('restify');
 var builder = require('botbuilder');
-var botbuilder_azure = require("botbuilder-azure");
+var azure = require("botbuilder-azure");
+
+var documentDbOptions = {
+    host: 'https://bmibotdb.documents.azure.com:443/    ', 
+    masterKey: 'LPJ0ldNetBnHEXJZQtNAPc7W3q6zEAn8alNtEauaHE7ZAfBIU93gRSfsKCoN5QrNKQK2ePWZMDzOkmVjidhuSg', 
+    database: 'botdocs',   
+    collection: 'botdata'
+};
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+var docDbClient = new azure.DocumentDbClient(documentDbOptions);
+
+var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said BMI: %s", session.message.text);
+}).set('storage', inMemoryStorage);
 
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+//var bot = new builder.UniversalBot(connector, function (session) {
+//    session.send("You said BMI: %s", session.message.text);
+//}).set('storage', cosmosStorage);
 
-// Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
-bot.set('storage', tableStorage);
-
-bot.dialog('/', function (session) {
-    session.send('You said to BMI telecom ' + session.message.text);
-});
